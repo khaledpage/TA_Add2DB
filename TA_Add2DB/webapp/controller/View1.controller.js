2940746e3a3b407f493f06e5462fae1a7ef80545
Khaledpage <khaledp16@gmail.com>
2020-05-29 17:18:49 +0000

    22

--- TA_Add2DB/webapp/controller/View1.controller.js
+++ TA_Add2DB/webapp/controller/View1.controller.js
@@ -6,6 +6,18 @@ sap.ui.define([
 	return Controller.extend("TA_logistik.TA_Add2DB.controller.View1", {
 		onInit: function () {
 
+		},
+		onAdd: function() {
+			// Get the values of the header input fields
+			var ComCode = this.getView().byId("inputName").getValue();
+			var Plant = this.getView().byId("inputVorname").getValue();
+			if (ComCode == "" && Plant == "") {
+			alert("Company Code and Plant cannot be blank");
+			}
+			alert(ComCode);
 		}
+		
+		
+		
 	});
 });
