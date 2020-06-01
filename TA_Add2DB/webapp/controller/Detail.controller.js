343cf3e72bbaa26f8acf93b120f8c0b028e821ce
Khaledpage <khaledp16@gmail.com>
2020-06-01 15:34:39 +0000

    list

---
+++ TA_Add2DB/webapp/controller/Detail.controller.js
@@ -0,0 +1,61 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("TA_logistik.TA_Add2DB.controller.Detail", {
+
+		/**
+		 * Called when a controller is instantiated and its View controls (if available) are already created.
+		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+		 * @memberOf TA_logistik.TA_Add2DB.view.Detail
+		 */
+		onInit: function () {
+var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			oRouter.getRoute("Detail").attachMatched(this._onRouteMatched, this);
+		},
+	_onRouteMatched: function (oEvent) {
+			var oArgs, oView;
+			oArgs = oEvent.getParameter("arguments");
+
+			var list = this.getView().byId("thelist");
+			var binding = list.getBinding("items");
+			binding.filter( [ new sap.ui.model.Filter([
+      new sap.ui.model.Filter("user_ID", sap.ui.model.FilterOperator.EQ, oArgs.productId )
+      //new sap.ui.model.Filter("gender", sap.ui.model.FilterOperator.Contains, query )
+   ],false)]);
+	},
+		handleNavButtonPress: function (evt) {
+				var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+				console.log("trying to go?");
+				oRouter.navTo("TargetView1");
+			}
+		/**
+		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
+		 * (NOT before the first rendering! onInit() is used for that one!).
+		 * @memberOf TA_logistik.TA_Add2DB.view.Detail
+		 */
+		//	onBeforeRendering: function() {
+		//
+		//	},
+
+		/**
+		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
+		 * This hook is the same one that SAPUI5 controls get after being rendered.
+		 * @memberOf TA_logistik.TA_Add2DB.view.Detail
+		 */
+		//	onAfterRendering: function() {
+		//
+		//	},
+
+		/**
+		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
+		 * @memberOf TA_logistik.TA_Add2DB.view.Detail
+		 */
+		//	onExit: function() {
+		//
+		//	}
+
+	});
+
+});
--- TA_Add2DB/webapp/controller/View1.controller.js
+++ TA_Add2DB/webapp/controller/View1.controller.js
@@ -1,6 +1,8 @@
+var global = "";
 sap.ui.define([
-	"sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel", "sap/ui/model/odata/v2/ODataModel"
-], function (Controller, JSONModel, ODataModel) {
+	"sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel", "sap/ui/model/odata/v2/ODataModel", "sap/m/MessageBox",
+	"sap/m/MessageToast"
+], function (Controller, JSONModel, ODataModel, MessageBox, MessageToast) {
 	"use strict";
 
 	return Controller.extend("TA_logistik.TA_Add2DB.controller.View1", {
@@ -40,9 +42,9 @@ sap.ui.define([
 			// var oModel = sap.ui.getCore().getModel();
 			// console.log(oModel);
 			var itemData = oModel.getProperty("/Users");
-			console.log(itemData);
+			// console.log(itemData);
 			var itemRow = {
-				ID: parseInt(ID,10),
+				ID: parseInt(ID, 10),
 				name: Name,
 				vorname: Vorname,
 				postzahl: Postzhal,
@@ -57,20 +59,86 @@ sap.ui.define([
 				Users: itemData
 			});
 			var oModelG = this.getView().getModel();
-			var oMetadata = oModelG.getServiceMetadata();
+			// var oMetadata = oModelG.getServiceMetadata();
+			// console.log(oMetadata);
 
-			console.log(oMetadata);
-			// oModel.create("/Users", itemRow);
 			jQuery.sap.require("sap.ui.commons.MessageBox");
 			oModelG.create('/Users', itemRow, null, function () {
 				sap.ui.commons.MessageBox.show(
 					// sap.ui.commons.MessageBox.alert("Success!");
+					"Success!"
 				);
-			}, function () {
-				sap.ui.commons.MessageBox.alert("Error!");
+
+			}, function (e) {
+				sap.ui.commons.MessageBox.alert("Error!:" + e);
+
 			});
 
+		},
+		setFilter: function (oContext) {
+			global = oContext.getSource().getText();
+			console.log(global);
+
+			// var oModelG = this.getView().getModel();
+
+			// var tf =	oModelG.read("/Packets", null, ["$filter=ID eq '2412341' "], false,
+			// 		function (oData, response) {
+			// 			//Sucess
+			// 		},
+			// 		function (oError) {}
+			// 	);
+			// 	console.log(tf);
+		},
+			handleListItemPress: function (oEvent) {
+			
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			var selectedProductId = oEvent.getSource().getBindingContext().getProperty("ID");
+			console.log("clicked ID:"+selectedProductId);
+			oRouter.navTo("Detail", {
+				productId: selectedProductId
+			});
 		}
+		// handelTableDetail: function (oContext) {
+		// 	var oModelG = this.getView().getModel();
+		// 	// var tf = oModelG.read("/Packets");
+		// 	// console.log(tf);
+		// 	// var oFilter = new sap.ui.model.Filter('ID',sap.ui.model.FilterOperator.EQ, global);
+
+		// 	// oContext.getSource().bindRows({  
+		// 	//                           path : '/Packets',
+		// 	//                           filters : [oFilter]
+		// 	//  });
+
+		// 	// var list = oContext.getSource();
+		// 	// 	var binding = list.getBinding("items");
+
+		// 	// 			var filters = [];
+		// 	// 				filters.push(oFilter);
+		// 	// 	binding.filter(filters);
+		// 	// 		// console.log(oContext.getSource().getBindingContext("Packets").getProperty("status")  );
+		// }
+		// getID: function (oContext) {
+		// 		console.log("am called");
+		// 		var filters = [];
+		// 		var oFilter = new sap.ui.model.Filter("ID", sap.ui.model.FilterOperator.EQ, global);
+		// 		var list = oContext.getSource();
+		// 		var binding = list.getBinding("items");
+		// 		filters.push(oFilter);
+		// 		binding.filter(filters);
+
+		// 	}
+			// handleDetail: function (oContext) {
+			// 	// create model filter
+			// 	var filters = [];
+
+		// 		var filter = new sap.ui.model.Filter("Packets", sap.ui.model.FilterOperator.Contains, global);
+		// 		filters.push(filter);
+
+		// 	// update list binding
+		// 	var list = oContext.getSource();
+		// 	var binding = list.getBinding("items");
+		// 	binding.filter(filters);
+		// }
 
 	});
 });
